f237bd3d3b21b3575bb0c38afad5ea8769091041
nickswilliam <nickswill2012@gmail.com>
2024-02-02 00:17:11 -0300

    modifying pre-header bar

--- src/components/Header/HeaderContact.tsx
+++ src/components/Header/HeaderContact.tsx
@@ -3,18 +3,21 @@ import WALogo from "@/assets/logo/whatsapp-logo.png";
 
 const HeaderContact = () => {
   return (
-    <header className="bg-blue-800/90 flex flex-col items-center justify-center md:flex-row md:h-12 md:justify-end px-10 py-2 gap-4 h-40 w-full z-40 relative">
-    <p className="text-violet-100 text-[18px] text-center">
-      ¿Dudas o consultas? Contactanos:
+    <header className="bg-blue-800/90 flex items-baseline justify-center md:flex-row md:h-12 md:justify-end px-2 sm:px-10 py-2 gap-4 w-full z-40 relative">
+    
+    <p className="text-violet-100 text-center hidden sm:block">
+      Contactanos:
     </p>
-    <a href="tel:+541122334455" className="text-orange-500 align-middle">
-      1122334455
+
+    <a href="tel:+541122334455" className="text-orange-500 flex items-center gap-1 justify-center">
+      <span className="text-violet-100">Tel:</span> 1122334455
     </a>
+
     <a
       href="https://api.whatsapp.com/send/?phone=541122334455"
-      className="flex gap-2 text-violet-100 bg-green-600 px-6 py-1 rounded-md"
+      className="flex gap-1 items-center justify-center text-violet-100 bg-green-600 px-2 py-1 rounded-md"
     >
-      <Image alt="wa-logo" src={WALogo} width={22} height={20} />
+      <Image alt="wa-logo" src={WALogo} width={undefined} className="w-4 h-4" />
       Whatsappeanos
     </a>
   </header>
