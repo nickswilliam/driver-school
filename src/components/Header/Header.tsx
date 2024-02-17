80dbe99e6c752e45bd09f47c671d8dd9f6e83655
nickswilliam <nickswill2012@gmail.com>
2024-02-17 05:43:35 -0300

    resize

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -35,7 +35,7 @@ export const Header = () => {
     };
 
     window.addEventListener("scroll", handleScroll);
-    /* window.addEventListener("resize", handleResize); */
+    window.addEventListener("resize", handleResize);
     return () => {
       window.removeEventListener("scroll", handleScroll);
       window.removeEventListener("resize", handleResize);
